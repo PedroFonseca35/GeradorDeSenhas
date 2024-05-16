04bfb91790cdc2a6098ac8c7037adfca36be9f3f
PedroFonseca35 <115945359+PedroFonseca35@users.noreply.github.com>
2024-05-15 21:48:52 -0300

    Versão 0.2
    Implementação js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-let inputTela = document.getElementById('');
+let inputNumero = document.getElementById('inputNumber');
+let inputTela = document.getElementById('inputRange');
+
+inputTela.addEventListener('input', function() {
+    inputNumero.value = inputTela.value;
+});
+
+inputNumero.addEventListener('input', function() {
+    inputTela.value = inputNumero.value;
+});
+
+function gerarSenha() {
+    document.addEventListener('DOMContentLoaded', function () {
+        var checkbox = this.documentElementById('box1');
+        if (checkbox.cheked) {
+            
+        }
+    });
+
+}
