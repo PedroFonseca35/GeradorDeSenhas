0e5ac204ead6cd8c3a6937a21603d570dc4047a7
PedroFonseca35 <115945359+PedroFonseca35@users.noreply.github.com>
2024-05-17 16:28:12 -0300

    versão 0.5
    Implementando funções

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let inputNumero = document.getElementById('inputNumber');
 let inputTela = document.getElementById('inputRange');
+let inputGeracao = document.getElementById('inputText');
 
 inputTela.addEventListener('input', function() {
     inputNumero.value = inputTela.value;
@@ -10,11 +11,44 @@ inputNumero.addEventListener('input', function() {
 });
 
 function gerarSenha() {
-    document.addEventListener('DOMContentLoaded', function () {
-        var checkbox = this.documentElementById('box1');
-        if (checkbox.cheked) {
-            
-        }
-    });
+    var checkbox = document.getElementById('box1');
+    if (checkbox.checked) {
+        let numero = gerarNumeroInteiro(0, 9);
+        inputGeracao.value = numero;
+    }
+    var checkbox2 = document.getElementById('box2');
+    if (checkbox2.checked) {
+        let caracter = caracterAleatorio();
+        inputGeracao.value = caracter; 
+    }
+    var checkbox3 = document.getElementById('box3');
+    if (checkbox3.checked) {
+        let letrasMinusculas = minuscula();
+        inputGeracao.value = letrasMinusculas;
+    }
+    var checkbox4 = document.getElementById('box4');
+    if (checkbox4.checked) {
+        let letrasMaiusculas = maiuscula();
+        inputGeracao.value = letrasMaiusculas;
+    }
+}
 
+function gerarNumeroInteiro(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function caracterAleatorio() {
+    let caracteres = '!@#$%¨&*()_-=+§{[]}~/?|';
+    let escolherAleatorio = Math.floor(Math.random() * caracteres.length);
+    return caracteres.charAt(escolherAleatorio);
+}
+
+function minuscula() {
+    let randomMinuscula = Math.floor(Math.random() * 26) + 97;
+    return String.fromCharCode(randomMinuscula);
+}
+
+function maiuscula() {
+    let randomMaiuscula = Math.floor(Math.random() * 26) + 65;
+    return String.fromCharCode(randomMaiuscula);
 }
